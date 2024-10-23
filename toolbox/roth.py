5bca13f4f33878c39909d3ecb83e59e46f63eb24
vincexma <vincexma@gmail.com>
2024-10-23 16:01:40 -0400

    NFC: roth: clean up and add comments

--- toolbox/roth.py
+++ toolbox/roth.py
@@ -1,12 +1,19 @@
+# Roth IRA value benchmark
+# calculates the expected value of a Roth IRA <yrs> years since opening
+# NB: this program assumes constant maximum annual contribution ($6000.00)
+
+# output: <yrs> <tsm> <value>
+#  yrs: years since initial investment, same as input
+#  tsm: total sum multiplier, the factor by which the total contribution has grown by
+#   NB: this is less than the growth of the first contribution, 1.1^n
+#  value: the projected/expected value of the IRA account after <yrs>
+
+
 roi = 1.1
-years = int(input("time in the market: "))
+time = int(input("Years in market: "))
 def project(yrs):
 	factor = (1. - roi ** (yrs + 1))/(1. - roi) - 1
-	# print('y' + str(yrs), 'x' + str(round((factor/yrs), 2)), '$' + str(round(6000. * factor, 2)))
-	print('(' + str(yrs) + ',' + str(round((factor/yrs), 2)) + ')')
+	print(yrs, round((factor/yrs), 3), round(6000. * factor, 2))
 
-#for i in [-5, -3, -1, 0, 1, 3, 5]:
-#	project(years + i)
+project(time)
 
-for i in range(2, 65):
-	project(i)
